7d1226f2b28cb065db465d84864e0f7b17ece694
Robi Irhamni <robidream20@gmail.com>
2022-09-17 19:51:14 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/robilaruku/go-say-hello
+module github.com/robilaruku/go-say-hello/v2
 
 go 1.17
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package golangmodulesayhello
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello" + name
 }
